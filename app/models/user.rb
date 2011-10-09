4e89ad99dab82486adde4d9ca200e17e8dfbd849
Michael Liu <superinsky@gmail.com>
2011-10-09 20:18:22 +1100

    Working user signin

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,8 +28,17 @@ class User < ActiveRecord::Base
   class << self 
     def authenticate(email, submitted_password)
       user = find_by_email(email)
-      return nil if user.nil?
-      return user if user.has_password?(submitted_password)
+      (user && user.has_password?(submitted_password)) ? user : nil
+      # the following 2 two line code equals the above one-liner
+      # return nil if user.nil?
+      # return user if user.has_password?(submitted_password)
+    end
+    
+    def authenticate_with_salt(id, cookie_salt)
+      user = find_by_id(id)
+      (user && user.salt == cookie_salt) ? user : nil
+      # if (the user return back) and (salt = salt) return user else 
+      # return nil
     end
   end
   
@@ -53,14 +62,17 @@ class User < ActiveRecord::Base
     end
 end
 
+
 # == Schema Information
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#  salt               :string(255)
 #
 
